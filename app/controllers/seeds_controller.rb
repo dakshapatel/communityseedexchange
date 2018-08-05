59820e5cf314f2a638c9313f1880db49591f298d
github username <github email address>
2018-08-05 15:49:15 -0700

    group instances in fruit, vegetable, herb catergory

--- app/controllers/seeds_controller.rb
+++ app/controllers/seeds_controller.rb
@@ -4,7 +4,6 @@ class SeedsController < ApplicationController
     authenticate_user
   end
 
- 
   get '/seeds' do
     session[:user_id]
     @seeds = Seed.all
@@ -12,60 +11,53 @@ class SeedsController < ApplicationController
     erb :'/seeds/index'
   end
 
+  get '/seeds:id' do
+    
+    erb :'seeds/post'
+  end 
+
   get '/seeds/new' do
-    authenticate_user
+   
     erb :"seeds/new"
   end
 
   post '/seeds' do
-    authenticate_user
+    
     @user = User.find_by(session[:user_id])
     @seed = Seed.create(:name => params[:name], :catergory => params[:catergory], :description => params[:description], :user_id => @user.id)
     redirect to "/seeds/#{@seed.id}"
   end
+
+  get '/seeds/user_seeds' do 
+    
+    @seeds = current_user.seeds
+    binding.pry
+    erb :'seeds/users_seeds'
+
+  end 
 
   get '/seeds/:id' do
     @seed = Seed.find_by(params[:id])
     erb :'seeds/show'
   end
 
-
-  get '/seeds/myseeds' do
-
-
-  end 
-  # get '/users_seeds' do 
-  #   @user = User.find_by(session[:user_id])
-    
-  #   @seed = Seed.create(:name => params[:name], :catergory => params[:catergory], :description => params[:description], :user_id => @user.id)
-  #   erb :'/seeds/users_seeds'
-  # end 
-
-  get '/seeds/:id' do
-    @seed = Seed.find_by(params[:id])
-    erb :'seeds/edit'
-  end
-
-
   get '/seeds/:id/edit' do
     @seed = Seed.find_by(params[:id])
     erb :'seeds/edit'
   end
 
-  patch '/seeds/:id/edit' do
+  patch '/seeds/:id' do
     @seed = Seed.find_by_id(params[:id])
     @seed.name = params[:name]
     @seed.save
     redirect to "/seeds/#{@seed.id}"
   end
 
-#destroy, should delete seed 1
   delete '/seeds/:id/delete' do
     @seed = Seed.find_by(params[:id])
     @seed.delete
 
     redirect to '/index'
   end
-
 
 end
